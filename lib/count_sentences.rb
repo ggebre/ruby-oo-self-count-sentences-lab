5324e4b37fdb7fc59fc98cc89cfba83706265203
Getu Gebre <getu.gebre.gg@gmail.com>
2020-10-13 09:41:28 -0500

    done

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -4,17 +4,18 @@ class String
 
   def sentence?
     
+      self.match(/\.$/) ? true : false
   end
 
   def question?
-
+    self.match(/\?$/) ? true : false
   end
 
   def exclamation?
-
+    self.match(/!$/) ? true : false
   end
-
+  # counts number of sentences 
   def count_sentences
-
+    self.scan(/\w+[.!?]/).length
   end
 end
